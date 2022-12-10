9d3235791d49215b10686d2246f6122041445174
wowvain-dev <wowvain.dev@gmail.com>
2022-12-10 16:29:25 +0200

    Started working on mobile navbar responsiveness. Will create new branch.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,20 @@
 <script>
 	import '../app.css';
-	import Navbar from "../components/Navbar.svelte";
-	import VianuLogo from "../components/VianuLogo.svelte";
+	import Navbar from '../components/Navbar.svelte';
+	import VianuLogo from '../components/VianuLogo.svelte';
 
-
-	import {page} from "$app/stores";
-	import {goto} from "$app/navigation";
-	import Footer from "../components/Footer.svelte";
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+	import Footer from '../components/Footer.svelte';
 
 	export const prerender = true;
 </script>
 
 <!--NAVBAR-->
 {#if $page.url.pathname === '/'}
-	<div></div>
+	<div />
 {:else}
-	<div>
+	<div style="z-index: 999">
 		<Navbar />
 	</div>
 {/if}
@@ -26,4 +25,6 @@
 </div>
 
 <!--FOOTER-->
-<Footer class="mt-10" />
+<div class="mt-10">
+	<Footer />
+</div>
