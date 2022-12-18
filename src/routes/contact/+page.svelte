b0226cae30f09556e332060b9a8b23e95471453d
wowvain-dev <wowvain.dev@gmail.com>
2022-12-19 01:53:19 +0200

    Added developer in the contact page.

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,51 +1,76 @@
 <script>
-
 </script>
 
 <div class="mt-[15vh] mb-[15vh]">
-    <div class="text-sm breadcrumbs ml-10">
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li>Contact</li>
-        </ul>
-    </div>
-    <h1 class="font-bold text-5xl text-center mt-12">Contact</h1>
+	<div class="text-sm breadcrumbs ml-10">
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li>Contact</li>
+		</ul>
+	</div>
+	<h1 class="font-bold text-5xl text-center mt-12">Contact</h1>
 
-    <div id="content" class="mx-[5vw] md:mx-[30vw]">
-        <p class="mt-24"><b>The official e-mail address of VianuMUN:</b> <code
-            class="lowercase text-lg font-medium"
-        >vianumun.contact@gmail.com</code></p> <br/>
+	<div id="content" class="mx-[5vw] md:mx-[30vw]">
+		<p class="mt-24">
+			<b>The official e-mail address of VianuMUN:</b>
+			<code class="lowercase text-lg font-medium">vianumun.contact@gmail.com</code>
+		</p>
+		<br />
 
-        <div class="flex-col">
-            <p class="flex-auto mt-10" ><b>Natalia Velea-Grumezea</b> - <i>Secretary General</i></p>
-            <p class="flex-auto font-serif mt-1">(RO) 0722 450 855 / (FR) +33 780 747 057</p>
-            <p class="flex-auto font-serif mt-1">nataliavege@gmail.com</p>
-        </div>
+		<div class="flex-col">
+			<p class="flex-auto mt-10"><b>Natalia Velea-Grumezea</b> - <i>Secretary General</i></p>
+			<p class="flex-auto font-serif mt-1">(RO) 0722 450 855 / (FR) +33 780 747 057</p>
+			<p class="flex-auto font-serif mt-1">nataliavege@gmail.com</p>
+		</div>
 
-        <div class="flex-col">
-            <p class="flex-auto mt-10" ><b>Mara Andronache</b> - <i>Deputy Secretary General</i></p>
-            <p class="flex-auto font-serif mt-1">(RO) 0734 941 562</p>
-            <p class="flex-auto font-serif mt-1">mararux05@gmail.com</p>
-        </div>
+		<div class="flex-col">
+			<p class="flex-auto mt-10"><b>Mara Andronache</b> - <i>Deputy Secretary General</i></p>
+			<p class="flex-auto font-serif mt-1">(RO) 0734 941 562</p>
+			<p class="flex-auto font-serif mt-1">mararux05@gmail.com</p>
+		</div>
 
-        <div class="flex-col">
-            <p class="flex-auto mt-10" ><b>Alexandra Breahnă</b> - <i>Conference Manager</i></p>
-            <p class="flex-auto font-serif mt-1">(RO) 0731 768 847</p>
-            <p class="flex-auto font-serif mt-1">alexandrabreahna@gmail.com</p>
-        </div>
+		<div class="flex-col">
+			<p class="flex-auto mt-10"><b>Alexandra Breahnă</b> - <i>Conference Manager</i></p>
+			<p class="flex-auto font-serif mt-1">(RO) 0731 768 847</p>
+			<p class="flex-auto font-serif mt-1">alexandrabreahna@gmail.com</p>
+		</div>
 
+		<div class="flex-col">
+			<p class="flex-auto mt-10"><b>Bogdan Stanciu</b> - <i>Web Developer</i></p>
+			<p class="flex-auto font-serif mt-1">
+				wowvain.dev@gmail.com (for bug reports and/or inquiries)
+			</p>
+			<div class="flex-auto font-serif mt-1 " style="flex-direction: row; display:flex;">
+				<span class="mr-5">Find me on:</span>
+				<a href="https://github.com/wowvain-dev"
+					><img
+						src="https://img.shields.io/badge/github-000000?style=for-the-badge&logo=github&logoColor=white&link=https://github.com/wowvain-dev"
+						alt=""
+					/></a
+				>
+			</div>
+			<div class="flex-auto font-serif mt-1 " style="flex-direction: row; display: flex">
+				<span class="mr-5">Made in:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+				<a href="https://svelte.dev"
+					><img
+						src="https://img.shields.io/badge/svelte-ff3e00?style=for-the-badge&logo=svelte&logoColor=white&link=https://svelte.dev/"
+						alt=""
+					/></a
+				>
+				<a href="https://tailwindcss.com"
+					><img
+						src="https://img.shields.io/badge/tailwindcss-38bdf8?style=for-the-badge&logo=tailwindcss&logoColor=white&link=https://tailwindcss.com/"
+						alt=""
+					/></a
+				>
+			</div>
+		</div>
 
-        <div class="flex-col">
-            <p class="flex-auto mt-10" ><b>Address</b>: Strada Arhitect Ion Mincu 10, Bucharest 011358</p>
-        </div>
-
-        <div class="flex-col">
-            <p class="flex-auto mt-20"><b>Developer (for bug reports and/or inquiries)</b>: wowvain.dev@gmail.com</p>
-        </div>
-    </div>
-
+		<div class="flex-col">
+			<p class="flex-auto mt-10"><b>Address</b>: Strada Arhitect Ion Mincu 10, Bucharest 011358</p>
+		</div>
+	</div>
 </div>
 
 <style>
-
 </style>
